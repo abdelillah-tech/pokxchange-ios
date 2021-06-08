2553c2710d83e552312fcb89e22e1ac3d67f0c9a
abdelillah ghomari <ghomari.professionnel@gmail.com>
2021-06-08 23:57:32 +0200

    Initial Commit

---
+++ pokxchange/pokxchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokxchangeApp.swift
+//  pokxchange
+//
+//  Created by abdelillah ghomari on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct pokxchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
